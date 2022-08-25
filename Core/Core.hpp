db0c8f350e6b663f23827006d2a277e158244d14
JustCabbage <47952062+JustCabbage@users.noreply.github.com>
2022-08-25 07:22:06 +0100

    Added ImGui Support

--- Core/Core.hpp
+++ Core/Core.hpp
@@ -1,45 +1,40 @@
 #pragma once
-#include "Emulator/CPU.hpp"
-#include <SFML/Graphics.hpp>
+#include "Emulator/Render.hpp"
 
 namespace Core
 {
     void Initialize(const std::string& ROMPath)
     {
         Emulator::CPU CPU(ROMPath);
-        sf::RenderWindow EmulatorWindow(sf::VideoMode({640, 320}), "CHIP-8 Emulator | github.com/JustCabbage", sf::Style::Close);
-        
+        sf::RenderWindow EmulatorWindow(sf::VideoMode({1280, 720}), "CHIP-8 Emulator | github.com/JustCabbage", sf::Style::Close);
+        ImGui::SFML::Init(EmulatorWindow);
+        sf::Clock Clock;
+
         while(EmulatorWindow.isOpen())
         {
-            sf::Event e;
-            while(EmulatorWindow.pollEvent(e))
+            sf::Event event;
+            while(EmulatorWindow.pollEvent(event))
             {
-                switch(e.type)
+                ImGui::SFML::ProcessEvent(event);
+                switch(event.type)
                 {
                 case sf::Event::Closed:
                     EmulatorWindow.close();
                     break;
                 }
             }
-
+            
+            ImGui::SFML::Update(EmulatorWindow, Clock.restart());
+            Renderer::RenderUI(CPU);
+            
             CPU.Cycle();
 
             EmulatorWindow.clear();
-            for(int i = 0; i < CPU.VideoBuffer.size(); i++)
-            {
-                for(int j = 0; j < CPU.VideoBuffer[i].size(); j++)
-                {
-                    if(CPU.VideoBuffer[i][j])
-                    {
-                        sf::RectangleShape Pixel;
-                        Pixel.setPosition({(float)(i * 10), (float)(j * 10)});
-                        Pixel.setSize({10,10});
-                        Pixel.setFillColor(sf::Color::Blue);
-                        EmulatorWindow.draw(Pixel);
-                    }
-                }
-            }
+            Renderer::RenderVideoBuffer(EmulatorWindow, CPU);
+            ImGui::SFML::Render(EmulatorWindow);
             EmulatorWindow.display();
         }
+
+        ImGui::SFML::Shutdown();
     }
 }
---
+++ Core/Emulator/Render.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <imgui-SFML.h>
+#include <imgui.h>
+#include "CPU.hpp"
+
+namespace Core::Renderer
+{
+    void RenderVideoBuffer(sf::RenderTarget& Target, Emulator::CPU& CPU)
+    {
+        for(int i = 0; i < CPU.VideoBuffer.size(); i++)
+        {
+            for(int j = 0; j < CPU.VideoBuffer[i].size(); j++)
+            {
+                if(CPU.VideoBuffer[i][j])
+                {
+                    sf::RectangleShape Pixel;
+                    Pixel.setPosition({(float)(i * 10), (float)(j * 10)});
+                    Pixel.setSize({10,10});
+                    Pixel.setFillColor(sf::Color::Blue);
+                    Target.draw(Pixel);
+                }
+            }
+        }
+    }
+
+    void RenderUI(Emulator::CPU& CPU)
+    {
+        ImGui::SetNextWindowSize({1280 - 640, 720});
+        ImGui::SetNextWindowPos({640, 0});
+        if(ImGui::Begin("Processor Information", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse))
+        {
+
+            ImGui::End();
+        }
+
+        ImGui::SetNextWindowSize({640, 720 - 320});
+        ImGui::SetNextWindowPos({0, 320});
+        if(ImGui::Begin("Configuration", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse))
+        {
+            ImGui::End();
+        }
+    }
+}

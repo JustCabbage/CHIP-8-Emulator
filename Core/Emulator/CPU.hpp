edabeb4579a63550e22f06f6563b664d88c4c40a
JustCabbage <47952062+JustCabbage@users.noreply.github.com>
2022-08-27 01:13:05 +0100

    Added more instruction support & Code cleanup

--- Core/Emulator/CPU.hpp
+++ Core/Emulator/CPU.hpp
@@ -1,7 +1,9 @@
 #pragma once
+#include <map>
 #include <array>
 #include <string>
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 namespace Core::Emulator
 {
@@ -9,17 +11,22 @@ namespace Core::Emulator
     {
     public:
         CPU(const std::string& ROMPath);
-        std::uint16_t GetCurrentOpCode();
-        std::array<std::uint8_t, 16> GetRegisters();
+        void Reset();
+        void HandleKeyEvent(sf::Event& Event);
+        void CycleTimers();
         void Cycle();
     public:
-        std::array<std::array<std::uint8_t, 32>, 64> VideoBuffer = {};
-    private:
-        std::uint16_t m_I = 0;
-        std::uint64_t m_Size = 0;
-        std::uint16_t m_CurrentOpCode = 0;
+        std::uint16_t I = 0;
+        std::uint64_t Size = 0;
+        std::uint8_t DelayTimer = 0;
+        std::uint64_t TotalTicks = 0;
+        std::uint8_t StackPointer = 0;
+        std::uint16_t CurrentOpCode = 0;
         std::uint16_t m_ProgramCounter = 0;
-        std::array<std::uint8_t, 4096> m_Memory = {};
-        std::array<std::uint8_t, 16> m_Registers = {};
+        std::array<std::uint16_t, 16> Stack = {};
+        std::map<std::uint16_t, bool> Keypad = {};
+        std::array<std::uint8_t, 4096> Memory = {};
+        std::array<std::uint8_t, 16> Registers = {};
+        std::array<std::array<std::uint8_t, 32>, 64> VideoBuffer = {};
     };
 }
